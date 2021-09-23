1e374c0d45c82523bff6530aed2f5202ca8db3f0
Will Szumski <will@stackhpc.com>
2021-09-23 14:12:56 +0000

    Validate load list to prevent acidental run with non-existing file

--- pipeline/tempest.sh
+++ pipeline/tempest.sh
@@ -11,6 +11,11 @@ function post_validate {
     if [ ! -z ${KAYOBE_AUTOMATION_TEMPEST_CONF_OVERRIDES:+x} ] && [ ! -f "${KAYOBE_AUTOMATION_TEMPEST_CONF_OVERRIDES}" ]; then
         die $LINENO "KAYOBE_AUTOMATION_TEMPEST_CONF_OVERRIDES: ${KAYOBE_AUTOMATION_TEMPEST_CONF_OVERRIDES} does not exist"
     fi
+    # Special case the default value of 'default'.
+    if [ "$(basename $KAYOBE_AUTOMATION_TEMPEST_LOADLIST_FULL_PATH$)" != "default" ] && \
+	    [ ! -f $KAYOBE_AUTOMATION_TEMPEST_LOADLIST_FULL_PATH ]; then
+       die $LINENO "KAYOBE_AUTOMATION_TEMPEST_LOADLIST_FULL_PATH: ${KAYOBE_AUTOMATION_TEMPEST_LOADLIST_FULL_PATH} does not exist"
+    fi
 }
 
 function post_config_set {

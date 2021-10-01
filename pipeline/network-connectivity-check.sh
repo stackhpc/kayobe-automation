4925f3718c1d14416fb23fe3822132476a7c54f8
Will Szumski <will@stackhpc.com>
2021-10-01 17:37:21 +0000

    Add additional scripts for various kayobe commands

---
+++ pipeline/network-connectivity-check.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -euE
+set -o pipefail
+
+PARENT="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+source "${PARENT}/../functions"
+
+function main {
+    kayobe_init
+    run_kayobe network connectivity check
+    pull_request "${KAYOBE_AUTOMATION_CONTEXT_ENV_PATH}/src/kayobe-config"
+}
+
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    main
+fi

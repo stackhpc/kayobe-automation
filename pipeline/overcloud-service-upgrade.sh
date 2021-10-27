aa81792b48e951cc0177b681faed3e3284cc6836
Will Szumski <will@stackhpc.com>
2021-10-27 08:37:22 +0000

    Add additional overcloud jobs

---
+++ pipeline/overcloud-service-upgrade.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -euE
+set -o pipefail
+
+PARENT="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+source "${PARENT}/../functions"
+
+function main {
+    kayobe_init
+    run_kayobe overcloud service upgrade
+    pull_request "${KAYOBE_AUTOMATION_CONTEXT_ENV_PATH}/src/kayobe-config"
+}
+
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    main
+fi

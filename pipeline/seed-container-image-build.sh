d013752bfaa0329e5c70d177600b841656deb65c
Will Szumski <will@stackhpc.com>
2022-05-05 16:05:34 +0000

    Add seed container image build

---
+++ pipeline/seed-container-image-build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -euE
+set -o pipefail
+
+PARENT="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+source "${PARENT}/../functions"
+
+function main {
+    kayobe_init
+    push_arg=""
+    # Validation in kayobe_init guarentees we can use numeric comparison.
+    if [ "${KAYOBE_AUTOMATION_PUSH_IMAGE}" -ne 0 ]; then
+        log_info "Images are configured to be pushed"
+        push_arg="--push"
+    fi
+    run_kayobe seed container image build "$@" $push_arg
+}
+
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    main "$@"
+fi
